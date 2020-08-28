db6808881542f991f5ec1e41a41ada845c97757c
jupiter0913 <jupiter0913@protonmail.com>
2020-08-28 20:11:43 +0200

    Update home page.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -26,20 +26,40 @@ export default function Home() {
           <img src='/Assets/Formes/illustration/vert-mini.png' className={styles.backgroundImage03} />
           <Container fluid className={styles.cardArea}>
             <Row className={styles.cardArea}>
-              <Col>
+              <Col xs={3}>
                 <div className={styles.card}>
+                  <div className={styles.cardContent}>
+                    <img src='/Assets/images/cloud.png' alt="cloud" />
+                    <p className={styles.subTitle}>Cloud</p>
+                    <p className={styles.text}>Optimiser l’utilisation de votre IT au quotidien avec des technologies agiles, sécurisées et innovantes.</p>
+                  </div>
                 </div>
               </Col>
-              <Col>
+              <Col xs={3}>
                 <div className={styles.card}>
+                  <div className={styles.cardContent}>
+                    <img src='/Assets/images/modern-workplace.png' alt="cloud" />
+                    <p className={styles.subTitle}>Modern Workplace</p>
+                    <p className={styles.text}>Mettre en place et interconnecter des solutions collaboratives pour vos équipes.</p>
+                  </div>
                 </div>
               </Col>
-              <Col>
+              <Col xs={3}>
                 <div className={styles.card}>
+                  <div className={styles.cardContent}>
+                    <img src='/Assets/images/sécurité.png' alt="cloud" />
+                    <p className={styles.subTitle}>Sécurité</p>
+                    <p className={styles.text}>Vous permettre d’être serein en protégeant votre cœur informatique ainsi que vos données  tout en garantissant la protection des données utilisateurs (RGPD friendly).</p>
+                  </div>
                 </div>
               </Col>
-              <Col>
+              <Col xs={3}>
                 <div className={styles.card}>
+                  <div className={styles.cardContent}>
+                    <img src='/Assets/images/service-managé.png' alt="cloud" />
+                    <p className={styles.subTitle}>Services Managés</p>
+                    <p className={styles.text}>Superviser votre système IT. Équiper des meilleurs outils pour superviser de façon complète votre SI</p>
+                  </div>
                 </div>
               </Col>
             </Row>
